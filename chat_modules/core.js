9a87fd90dba6f876b02935ddff684bc8ef89fa46
The-Quill <seriouslyquill@gmail.com>
2016-05-15 20:01:47 +1000

    Fixed promise loading

--- chat_modules/core.js
+++ chat_modules/core.js
@@ -1,9 +1,10 @@
-const cheerio = require('cheerio');
-const Promise = require("bluebird");
-const request = Promise.promisifyAll(require('request'));
-const config = require('./config');
-const WebSocket = require('ws');
-const ChatHandler = require('./chat');
+"use strict";
+var cheerio = require("cheerio");
+var Promise = require("bluebird");
+var request = Promise.promisifyAll(require("request"));
+var config = require("./config");
+var WebSocket = require("ws");
+var ChatHandler = require("./chat");
 
 var domainVars = {
     fkey: {},
@@ -11,24 +12,24 @@ var domainVars = {
 };
 var actions = {
     join: function(domain, roomId, fkey) {
-        fkey = fkey || domainVars.fkey[domain];
+        var fkeyProper = fkey || domainVars.fkey[domain];
         return request.postAsync({
-            url: 'http://chat.' + domain + '.com/ws-auth',
+            url: "http://chat." + domain + ".com/ws-auth",
             form: {
-                fkey: fkey,
+                fkey: fkeyProper,
                 roomid: roomId
             },
             jar: domainVars.jars[domain]
         });
     },
-    leave: function(domain, roomId){
+    leave: function(domain, roomId) {
         if (!domain || !roomId) {
             console.log("You're missing a part of that argument");
             return;
         }
-        fkey = domainVars.fkey[domain];
+        var fkey = domainVars.fkey[domain];
         return request.postAsync({
-            url: 'http://chat.' + domain + '.com/chats/leave/' + roomId,
+            url: "http://chat." + domain + ".com/chats/leave/" + roomId,
             form: {
                 fkey: fkey
             },
@@ -41,36 +42,36 @@ var actions = {
             return;
         }
         request.post({
-            url: 'http://chat.' + domain + '.com/chats/' + roomId + '/messages/new',
+            url: "http://chat." + domain + ".com/chats/" + roomId + "/messages/new",
             form: {
                 fkey: domainVars.fkey[domain],
-                text: prefix ? prefix + ' ' + text : text
+                text: prefix ? prefix + " " + text : text
             },
             jar: domainVars.jars[domain]
         });
     },
-    star: function(domain, messageId){
+    star: function(domain, messageId) {
         if (!domain || !messageId) {
             console.log("You're missing a part of that command");
             return;
         }
-        fkey = domainVars.fkey[domain];
+        var fkey = domainVars.fkey[domain];
         return request.postAsync({
-            url: 'http://chat.' + domain + '.com/messages/' + messageId + '/star',
+            url: "http://chat." + domain + ".com/messages/" + messageId + "/star",
             form: {
                 fkey: fkey
             },
             jar: domainVars.jars[domain]
         });
     },
-    edit: function(domain, messageId, text){
+    edit: function(domain, messageId, text) {
         if (!domain || !messageId || !text) {
             console.log("You're missing a part of that command");
             return;
         }
-        fkey = domainVars.fkey[domain];
+        var fkey = domainVars.fkey[domain];
         return request.postAsync({
-            url: 'http://chat.' + domain + '.com/messages/' + messageId,
+            url: "http://chat." + domain + ".com/messages/" + messageId,
             form: {
                 fkey: fkey,
                 text: text
@@ -78,14 +79,14 @@ var actions = {
             jar: domainVars.jars[domain]
         });
     },
-    delete: function(domain, messageId){
+    delete: function(domain, messageId) {
         if (!domain || !messageId) {
             console.log("You're missing a part of that command");
             return;
         }
-        fkey = domainVars.fkey[domain];
+        var fkey = domainVars.fkey[domain];
         return request.postAsync({
-            url: 'http://chat.' + domain + '.com/messages/' + messageId + '/delete',
+            url: "http://chat." + domain + ".com/messages/" + messageId + "/delete",
             form: {
                 fkey: fkey
             },
@@ -93,43 +94,45 @@ var actions = {
         });
     }
 };
-function openWebSocket(url, time, domain) {
+var openWebSocket = function(url, time, domain) {
     return new Promise(function(resolve, reject) {
-        const ws = new WebSocket(url + "?l=" + time, null, {
+        var ws = new WebSocket(url + "?l=" + time, null, {
             headers: {
-                'Origin': 'http://chat.' + domain + '.com'
+                "Origin": "http://chat." + domain + ".com"
             }
         });
-        ws.on('message', function(jsonMessage) {
-            var message = JSON.parse(jsonMessage)
-            var keys = Object.keys(message)
+        ws.on("message", function(jsonMessage) {
+            var message = JSON.parse(jsonMessage);
+            var keys = Object.keys(message);
             keys.forEach(function(key) {
-                message[key].e && message[key].e.forEach(ChatHandler.processEvent);
+                if (message[key].e) { // eslint-disable-line id-length
+                    message[key].e.forEach(ChatHandler.processEvent); // eslint-disable-line id-length
+                }
             });
         });
-        ws.on('open', function() {
+        ws.on("open", function() {
             resolve(domain);
         });
-        ws.on('error', function() {
-            reject(e);
+        ws.on("error", function(error) {
+            reject(error);
         });
     });
 };
 
-var connectDomainRooms = function(domain, initialRoom, rooms) {
+var connectDomainRooms = function(domainMultiCase, initialRoom, rooms) {
     var initialRoomId = initialRoom.room_id;
-    domain = domain.toLowerCase();
+    var domain = domainMultiCase.toLowerCase();
     domainVars.jars[domain] = request.jar();
 
-    // Return a promise here
     return request.getAsync({
-        url: 'https://' + (domain == "stackexchange" ? config.default_se_to_login_into + "." : "") + domain + '.com/users/login',
+        url: "https://" + (domain === "stackexchange" ? config.default_se_to_login_into + "." : "") + domain + ".com/users/login",
         jar: domainVars.jars[domain]
-    }).then(function(response) {
-        var $ = cheerio.load(response.body);
-        var fkey = $("input[name=fkey]").attr('value');
+    })
+    .then(function(response) {
+        var dom = cheerio.load(response.body);
+        var fkey = dom("input[name=fkey]").attr("value");
         return request.postAsync({
-            url: 'https://' + (domain == "stackexchange" ? config.default_se_to_login_into + "." : "") + domain + '.com/users/login',
+            url: "https://" + (domain === "stackexchange" ? config.default_se_to_login_into + "." : "") + domain + ".com/users/login",
             form: {
                 email: config.user.email,
                 password: config.user.password,
@@ -137,61 +140,67 @@ var connectDomainRooms = function(domain, initialRoom, rooms) {
             },
             jar: domainVars.jars[domain]
         });
-    }).then(function(response) {
+    })
+    .then(function() {
         return request.getAsync({
-            url: 'http://chat.' + domain + '.com/rooms/' + initialRoomId + '/events',
+            url: "http://chat." + domain + ".com/rooms/" + initialRoomId + "/events",
             jar: domainVars.jars[domain]
         });
-    }).then(function(response) {
-        var $ = cheerio.load(response.body)
-        var fkey = $("input[name=fkey]").attr('value');
+    })
+    .then(function(response) {
+        var dom = cheerio.load(response.body);
+        var fkey = dom("input[name=fkey]").attr("value");
         domainVars.fkey[domain] = fkey;
-        var url;
-        var time;
+        var url; // eslint-disable-line init-declarations
+        var time; // eslint-disable-line init-declarations
         request.postAsync({
-            url: 'http://chat.' + domain + '.com/ws-auth',
+            url: "http://chat." + domain + ".com/ws-auth",
             form: {
                 fkey: fkey,
                 roomid: initialRoomId
             },
             jar: domainVars.jars[domain]
-        }).then(function(response) {
-            if (response.body.indexOf("<!DOCTYPE HTML PUBLIC") !== -1) {
-                throw "There was an issue with the response. Check your config is correct.";
+        })
+        .then(function(secondLevelResponse) {
+            if (secondLevelResponse.body.indexOf("<!DOCTYPE HTML PUBLIC") !== -1) {
+                throw new Error("There was an issue with the response. Check your config is correct.");
             }
-            url = JSON.parse(response.body).url;
-            Object.keys(rooms).forEach(function(room) {
-                room = rooms[room];
-                debug("Connecting to " + room.name);
-                actions.join(domain, room.room_id, fkey);
+            url = JSON.parse(secondLevelResponse.body).url;
+            var connectionPromises = Object.keys(rooms).map(function(roomName) {
+                return new Promise(function(resolve, reject){
+                    var room = rooms[roomName];
+                    console.log("Connected to " + room.name);
+                    return actions.join(domain, room.room_id, fkey)
+                    .then(function(){
+                        resolve();
+                    })
+                    .error(function(){
+                        reject();
+                    });
+                });
             });
-            debug("Connecting to " + initialRoom.name);
-            return request.postAsync({
-                url: 'http://chat.' + domain + '.com/ws-auth',
+            connectionPromises.push(request.postAsync({
+                url: "http://chat." + domain + ".com/ws-auth",
                 form: {
                     fkey: fkey,
                     roomid: initialRoomId
                 },
                 jar: domainVars.jars[domain]
-            });
-        }).then(function(response) {
+            }));
+            return Promise.all(connectionPromises);
+        })
+        .then(function() {
             time = Math.floor(Date.now() / 1000);
             return openWebSocket(url, time, domain)
-            .then(function(){
-                debug('Opened a connection to ' + domain);
+            .then(function() {
+                console.log("Connected to " + domain);
             })
-            .catch(function(err){
+            .catch(function(err) {
                 console.error(err);
             });
         });
     });
 };
-
-function debug(message) {
-    if (config.debug) {
-        console.log(message);
-    }
-}
 var chatAbbreviationToFull = function(domainAbbreviation) {
     if (typeof domainAbbreviation !== "string") throw new Error("Abbeviation must be a string");
     return {
@@ -200,29 +209,30 @@ var chatAbbreviationToFull = function(domainAbbreviation) {
         se: "stackexchange"
     }[domainAbbreviation.toString().toLowerCase()];
 };
-var domainNameFixer = function(name){
-    name = name.replace(" ", "").toLowerCase();
-    if (name == "metastackexchange"
-    ||  name == "mse"){  return "meta.stackexchange" }
-    if (name == "so") {  return "stackoverflow"      }
-    if (name == "se") {  return "stackexchange"      }
+var domainNameFixer = function(unfixedName) {
+    var name = unfixedName.replace(" ", "").toLowerCase();
+    if (name === "metastackexchange" || name === "mse") {
+        return "meta.stackexchange";
+    } else if (name === "so") {
+        return "stackoverflow";
+    } else if (name === "se") {
+        return "stackexchange";
+    }
     return name;
-}
+};
 var start = function() {
-    const promises = Object.keys(config.room_domains).map(function(domainName) {
+    var promises = Object.keys(config.room_domains).map(function(domainName) {
         var domain = config.room_domains[domainName];
-        if (domain.rooms.length === 0){
+        if (domain.rooms.length == 0) {
             return new Promise()
-            .then(function(){})
-            .error(function(){});
+            .then()
+            .error();
         }
-        domainName = domainNameFixer(domainName);
-        var firstDomainName = Object.keys(domain.rooms)[0];
-        var domainRooms = JSON.parse(JSON.stringify(domain.rooms));
-        delete domainRooms[firstDomainName];
-        return connectDomainRooms(domainName, domain.rooms[firstDomainName], domainRooms);
-    })
-    return Promise.all(promises)
+        var domainNameFixed = domainNameFixer(domainName);
+        var firstDomain = domain.rooms[Object.keys(domain.rooms)[0]];
+        return connectDomainRooms(domainNameFixed, firstDomain, domain.rooms);
+    });
+    return Promise.all(promises);
 };
 module.exports = {
     actions: actions,
