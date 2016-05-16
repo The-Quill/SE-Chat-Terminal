ef2c3766472204a2c9271e107e1c47834a4e6000
Quill <seriouslyquill@gmail.com>
2016-05-17 09:38:42 +1000

    worked on

--- prompt.js
+++ prompt.js
@@ -153,6 +153,7 @@ var commands = {
         });
     },
     say: function(chatDomainUnfixed, roomId, message) {
+        console.log(arguments);
         var chatDomain = core.chatAbbreviationToFull(chatDomainUnfixed);
         core.actions.send(chatDomain, roomId, message);
     },
@@ -204,7 +205,7 @@ var handleInput = function(STDIN){
     var previousPromise = Promise.resolve();
     var input = STDIN.replace('/', '');
     var commandArgs = input.split(' ');
-    var commandName = commandArgs.splice(0, 1);
+    var commandName = commandArgs.splice(0, 1)[0];
     var commandArgsIndex = 0;
     if (Object.keys(properties).indexOf(commandName) === -1){
         console.log(Object.keys(properties).indexOf(commandName))
@@ -219,11 +220,23 @@ var handleInput = function(STDIN){
             resolve()
         });
     }
-    Object.keys(properties[commandName]).map(function(subCommandName){
+    var subCommandProperties = Object.keys(properties[commandName]);
+    if (commandArgs.length > subCommandProperties.length){
+        var potentialStringArray = commandArgs.splice(subCommandProperties.length - 1);
+        commandArgs[subCommandProperties.length - 1] = potentialStringArray.join(' ');
+    }
+    for (var i = 0; i < subCommandProperties.length; i++){
+        var subCommandName = subCommandProperties[i];
+        console.log(subCommandProperties);
         var subCommand = properties[commandName][subCommandName];
         previousPromise = previousPromise.then(function(){
             return new Promise(function(resolve, reject){
                 if (commandArgs.length > commandArgsIndex){
+                    console.log(subCommandName, commandArgs[commandArgsIndex]);
+                    if (Object.keys(subCommand).indexOf('pattern') === -1 && Object.keys(subCommand).indexOf('message') === -1){
+                        storedValues[subCommandName] = response;
+                        resolve(response);
+                    }
                     if (commandArgs[commandArgsIndex].match(subCommand.pattern) != null){
                         storedValues[subCommandName] = commandArgs[commandArgsIndex++];
                         resolve(commandArgs[commandArgsIndex])
@@ -249,13 +262,16 @@ var handleInput = function(STDIN){
                 }
             });
         });
-    });
+    }
     previousPromise.then(function(){
         // The null needs to be used here so the scope of the function isn't defined.
         // Read here for more info: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Function/apply
-        commands[input].apply(null, Object.keys(storedValues).map(function(valueName){
+        commands[commandName].apply(null, Object.keys(storedValues).map(function(valueName){
             return storedValues[valueName];
         }));
+    });
+    return new Promise(function(resolve){
+        resolve()
     });
 }
 
